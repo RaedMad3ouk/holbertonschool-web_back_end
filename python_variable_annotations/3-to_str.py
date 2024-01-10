eea53293e0eff348affc924414c5a54e3276d3e9
raed <raed.da3k@gmail.com>
2024-01-10 21:51:50 +0100

    str convert

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""a type-annotated function"""
+import math
+
+
+def to_str(n: float) -> str:
+    """Convert a float number to its string representation."""
+    return str(n)
