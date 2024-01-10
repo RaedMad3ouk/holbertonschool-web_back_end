acb077e28f59d3a29770a63c196d3c414a2d2074
raed <raed.da3k@gmail.com>
2024-01-10 21:57:45 +0100

    sum

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Complex types - list of floats"""
+from typing import List
+
+
+
+def sum_list(input_list: List[float]) -> float:
+    """return sum of list element"""
+    sum: float = 0
+    x: int = 0
+    for x in range(len(input_list)):
+        sum = sum + input_list[x]
+    return sum
