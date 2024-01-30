cfa81ed5b667416e56ed72535212503238e193d3
raed <raed.da3k@gmail.com>
2024-01-30 17:49:25 +0100

    loop

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+	const NewArra = [];
+	for (const idx of array) {
+	  NewArra.push(appendString + idx);
+	}
+  
+	return NewArra;
+  }
