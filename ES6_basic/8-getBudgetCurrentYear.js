db74eba72ff936a6a49f5879737fd75a2dc66a49
raed <raed.da3k@gmail.com>
2024-03-20 12:28:16 +0100

    add

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -1,15 +1,15 @@
 function getCurrentYear() {
-	const date = new Date();
-  
-	return date.getFullYear();
-  }
-  
-  export default function getBudgetForCurrentYear(income, gdp, capita) {
-	const budgets = {
-	  [`income-${getCurrentYear()}`]: income,
-	  [`gdp-${getCurrentYear()}`]: gdp,
-	  [`capita-${getCurrentYear()}`]: capita,
-	};
-  
-	return budgets;
-  }
+  const date = new Date();
+
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budgets = {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
+
+  return budgets;
+}
